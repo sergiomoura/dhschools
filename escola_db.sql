12e0ead8aa0de3290d61b6d5a3b5e64461b2b722
Sérgio Moura <smouracalmon@gmail.com>
2021-06-23 12:29:59 -0300

    Adicionada tela que lista areas

---
+++ escola_db.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE escola;
+USE escola;
+
+CREATE TABLE `alunos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) NOT NULL,
+  `sobrenome` varchar(200) NOT NULL,
+  `ano_matricula` int NOT NULL,
+  PRIMARY KEY (`id`)
+) CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `professores` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) NOT NULL,
+  `sobrenome` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `areas` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(100) NOT NULL,
+  `ano_matricula` int NOT NULL,
+  PRIMARY KEY (`id`)
+) CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `cursos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) NOT NULL,
+  `area_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY area_foreing (area_id) REFERENCES areas(id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `turmas` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `duracao` int(10) NOT NULL,
+  `ano_inicio` int(10) NOT NULL,
+  `semestre` tinyint(1) NOT NULL,
+  `curso_id` int(10) unsigned NOT NULL,
+  `professor_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY curso_foreing (curso_id) REFERENCES cursos(id),
+  FOREIGN KEY professor_foreing (professor_id) REFERENCES professores(id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `alunos_has_turmas` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `aluno_id` int(10) unsigned NOT NULL,
+  `turma_id` int(10) unsigned NOT NULL,
+  `numero_faltas` int(10),
+  PRIMARY KEY (`id`),
+  FOREIGN KEY aluno_foreing (aluno_id) REFERENCES alunos(id),
+  FOREIGN KEY turma_foreing (turma_id) REFERENCES turmas(id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/',(req,res,next) => {res.render('index',{title:'DHSchools'})})
-router.get('/home',(req,res) => {res.render('home')})
-router.get('/alunos',(req,res) => {res.render('alunos/alunos-index')})
-router.get('/alunos/create',(req,res) => {res.render('alunos/alunos-create')})
+router.get('/',(req,res,next) => {res.render('index',{title:'DHSchools'})});
+router.get('/home',(req,res) => {res.render('home')});
+router.get('/alunos',(req,res) => {res.render('alunos/alunos-index')});
+router.get('/alunos/create',(req,res) => {res.render('alunos/alunos-create')});
+router.get('/areas',(req,res) => {res.render('areas/areas-index')});
 
 module.exports = router;
