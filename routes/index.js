f6a2f882f41d44b761622bcce63588c3d130934e
Sérgio Moura <smouracalmon@gmail.com>
2021-06-23 09:33:24 -0300

    Criada tela que lista/busca alunos

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/',(req,res,next) => {res.render('index',{title:'DHSchools'})})
 router.get('/home',(req,res) => {res.render('home')})
+router.get('/alunos',(req,res) => {res.render('alunos/alunos-index')})
 
 module.exports = router;
