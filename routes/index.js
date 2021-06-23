c88d60a0ee471d2b3890056530e8c046fe338176
Sérgio Moura <smouracalmon@gmail.com>
2021-06-23 10:49:03 -0300

    Bugfix de caminhos para arquivos public

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ var router = express.Router();
 router.get('/',(req,res,next) => {res.render('index',{title:'DHSchools'})})
 router.get('/home',(req,res) => {res.render('home')})
 router.get('/alunos',(req,res) => {res.render('alunos/alunos-index')})
+router.get('/alunos/create',(req,res) => {res.render('alunos/alunos-create')})
 
 module.exports = router;
