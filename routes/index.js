5c17624b45e963b022c8effcd4506c6e2435739c
Sérgio Moura <smouracalmon@gmail.com>
2021-06-28 10:49:11 -0300

    Tela de CRUD para cursos

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ router.get('/home',(req,res) => {res.render('home')});
 router.get('/alunos',(req,res) => {res.render('alunos/alunos-index')});
 router.get('/alunos/create',(req,res) => {res.render('alunos/alunos-create')});
 router.get('/areas',(req,res) => {res.render('areas/areas-index')});
+router.get('/cursos',(req,res) => {res.render('cursos/cursos-index')});
 
 module.exports = router;
