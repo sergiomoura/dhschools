3cac3d9fdfd1364d0826aa95d49def55c43a2803
Sérgio Moura <smouracalmon@gmail.com>
2021-06-30 04:34:43 -0300

    Resetando rotas

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/',(req,res,next) => {res.render('index',{title:'DHSchools'})});
-router.get('/home',(req,res) => {res.render('home')});
-router.get('/alunos',(req,res) => {res.render('alunos/alunos-index')});
-router.get('/alunos/create',(req,res) => {res.render('alunos/alunos-create')});
-router.get('/areas',(req,res) => {res.render('areas/areas-index')});
-router.get('/cursos',(req,res) => {res.render('cursos/cursos-index')});
-router.get('/turmas',(req,res) => {res.render('turmas/turmas-index')});
-router.get('/professores',(req,res) => {res.render('professores/professores-index')});
+router.get('/',(_req,res,next) => {res.render('index',{title:'DHSchools'})});
 
 module.exports = router;
